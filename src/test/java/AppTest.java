9f52a2a7d9a18148ad8edcbde0a82a50349a4230
Gulushai <gulushai@gmail.com>
2019-08-06 22:35:44 -0400

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,9 @@
-public class AppTest {
+ipublic class AppTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("Good evening");
 
     }
 }
